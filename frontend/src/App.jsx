11accc23eaa1a85d2f23a51dd9313cbed3b9289d
Ivan <nguyenan18385@gmail.com>
2026-02-09 23:09:46 +0700

    feat: config app routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import CreateEvent from './pages/CreateEvent/CreateEvent';
 
 function App() {
   return (
-    <>
-      <h1>SWP391</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/create-event" replace />} />
+      <Route path="/create-event" element={<CreateEvent />} />
+    </Routes>
   );
 }
 
