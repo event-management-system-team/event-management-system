18d181f4a50acb14c9ac99ae646a3a311d181fef
ngtan07 <ngthanhan0712@gmail.com>
2026-03-04 12:22:45 +0700

    fix: UI Banner Apply

--- frontend/src/components/domain/event-detail/BannerApply.jsx
+++ frontend/src/components/domain/event-detail/BannerApply.jsx
@@ -18,6 +18,8 @@ const BannerApply = ({ eventSlug }) => {
         .join(', ');
     return (
         <div className="border-2 border-teal-accent bg-brand-teal/5 p-8 rounded-3xl flex flex-col md:flex-row items-center justify-between gap-6">
+
+
             <div className="flex items-start gap-4">
                 <div className="size-12 bg-teal-accent rounded-full flex items-center justify-center text-white shrink-0">
                     <Briefcase className="w-6 h-6" />
@@ -29,11 +31,15 @@ const BannerApply = ({ eventSlug }) => {
                         {recruitment.length > 3 && " and more..."}
                     </p>
                 </div>
-                <button className="bg-teal-accent hover:bg-teal-accent/90 text-white font-bold px-8 py-3 rounded-full transition-all shrink-0"
-                    onClick={() => navigate(`/recruitments/${eventSlug}`)}>
-                    Apply Now
-                </button>
             </div>
+
+            <button
+                className="bg-teal-accent hover:bg-teal-accent/90 text-white font-bold px-8 py-3 rounded-full transition-all shrink-0"
+                onClick={() => navigate(`/recruitments/${eventSlug}`)}
+            >
+                Apply Now
+            </button>
+
         </div>
     )
 }
