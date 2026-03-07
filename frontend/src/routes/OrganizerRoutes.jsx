9fe07f46a256fd816b95a568d74baa27e73f771f
Ivan <nguyenan18385@gmail.com>
2026-03-07 19:03:14 +0700

    config: set up routh path for organizer dashboard

--- frontend/src/routes/OrganizerRoutes.jsx
+++ frontend/src/routes/OrganizerRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "../components/common/ProtectedRoute";
 import OrganizerLayout from "../components/layout/OrganizerLayout";
 
+import OrganizerDashboardPage from "../pages/organizer/OrganizerDashboardPage";
 import MyEventsPage from "../pages/organizer/MyEventsPage";
 import CreateEventPage from "../pages/organizer/CreateEventPage";
 
@@ -10,6 +11,7 @@ const OrganizerRoutes = () => {
         <Routes>
             <Route element={<ProtectedRoute allowedRoles={["ORGANIZER"]} />}>
                 <Route element={<OrganizerLayout />}>
+                    <Route path="/dashboard" element={<OrganizerDashboardPage />} />
                     <Route path="/my-events" element={<MyEventsPage />} />
                     <Route path="/create-event" element={<CreateEventPage />} />
                 </Route>
