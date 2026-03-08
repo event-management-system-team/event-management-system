00ad289ef95da17b94688625f3e92b5fad2eac87
ngtan07 <ngthanhan0712@gmail.com>
2026-03-08 15:13:49 +0700

    feat: auto change status of recruitment when over deadline

--- backend/src/main/java/com/eventmanagement/backend/service/EventStatusJob.java
+++ backend/src/main/java/com/eventmanagement/backend/service/EventStatusJob.java
@@ -1,8 +1,8 @@
 package com.eventmanagement.backend.service;
 
 import com.eventmanagement.backend.repository.EventRepository;
+import com.eventmanagement.backend.repository.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +11,14 @@ import org.springframework.stereotype.Component;
 public class EventStatusJob {
 
     private final EventRepository eventRepository;
+    private final RecruitmentRepository recruitmentRepository;
 
     @Scheduled(fixedRate = 300000)
     public void autoUpdateEventStatuses() {
 
-        int ongoingUpdated = eventRepository.updateStatusToOngoing();
-        int completedUpdated = eventRepository.updateStatusToCompleted();
+        eventRepository.updateStatusToOngoing();
+        eventRepository.updateStatusToCompleted();
+        recruitmentRepository.updateStatusToClosed();
 
     }
 }
