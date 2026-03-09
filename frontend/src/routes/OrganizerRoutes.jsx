93017c7090c17c7d46b40225578b85608346cfd3
Ivan <nguyenan18385@gmail.com>
2026-03-09 09:19:14 +0700

    config: set up route for detail dashboard

--- frontend/src/routes/OrganizerRoutes.jsx
+++ frontend/src/routes/OrganizerRoutes.jsx
@@ -5,6 +5,7 @@ import OrganizerLayout from "../components/layout/OrganizerLayout";
 import OrganizerDashboardPage from "../pages/organizer/OrganizerDashboardPage";
 import MyEventsPage from "../pages/organizer/MyEventsPage";
 import CreateEventPage from "../pages/organizer/CreateEventPage";
+import EventDetailDashboard from "../pages/organizer/EventDetailDashboard";
 
 const OrganizerRoutes = () => {
     return (
@@ -14,6 +15,7 @@ const OrganizerRoutes = () => {
                     <Route path="/dashboard" element={<OrganizerDashboardPage />} />
                     <Route path="/my-events" element={<MyEventsPage />} />
                     <Route path="/create-event" element={<CreateEventPage />} />
+                    <Route path="/events/:eventId" element={<EventDetailDashboard />} />
                 </Route>
             </Route>
         </Routes>
