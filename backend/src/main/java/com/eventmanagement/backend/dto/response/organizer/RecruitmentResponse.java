5d7a57a9ad0ccb6500c19ec71e8052ca08503d31
tuikobt <leminhphuong15032005@gmail.com>
2026-03-16 01:14:45 +0700

    feat: update recruitment repository

--- backend/src/main/java/com/eventmanagement/backend/dto/response/organizer/RecruitmentResponse.java
+++ backend/src/main/java/com/eventmanagement/backend/dto/response/organizer/RecruitmentResponse.java
@@ -1,5 +1,40 @@
 package com.eventmanagement.backend.dto.response.organizer;
 
+import com.eventmanagement.backend.constants.RecruitmentStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RecruitmentResponse {
 
+    private UUID recruitmentId;
+
+    private UUID eventId;
+    private String eventName;
+
+    private UUID formId;
+    private String formName;
+
+    private String positionName;
+    private String description;
+    private Integer vacancy;
+    private Integer approvedCount;
+
+    private List<String> requirements;
+    private List<String> benefits;
+
+    private LocalDateTime deadline;
+    private RecruitmentStatus status;
+
+    private Integer remainingSlots;
+    private boolean isDeadlinePassed;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
